f40463e45ed5c85669337d281080fdda679adef0
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 15:19:34 -0300

    fix: refactor build application add script to build image

--- src/scripts/build_image.sh
+++ src/scripts/build_image.sh
@@ -2,5 +2,5 @@
 BUILD_IMAGE_IMAGE_NAME="$(echo "${BUILD_IMAGE_IMAGE_NAME}" | circleci env subst)"
 BUILD_IMAGE_IMAGE_TAG="$(echo "${BUILD_IMAGE_IMAGE_TAG}" | circleci env subst)"
 
-echo "Building docker image with identification\n: ${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}"
+echo "Building docker image with identification: ${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}"
 docker build -t "${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}" .
