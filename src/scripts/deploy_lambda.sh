23fe9218dcdc156a073d6aebfa4990fdf10b503a
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 16:28:30 -0300

    feat: generate apigw and deploy lambda

--- src/scripts/deploy_lambda.sh
+++ src/scripts/deploy_lambda.sh
@@ -3,7 +3,7 @@ DEPLOY_LAMBDA_PATH="$(echo "${DEPLOY_LAMBDA_PATH}" | circleci env subst)"
 DEPLOY_LAMBDA_DOTENV="$(echo "${DEPLOY_LAMBDA_DOTENV}" | circleci env subst)"
 
 echo "Installing chalice"
-cd ${DEPLOY_LAMBDA_PATH}
+cd "${DEPLOY_LAMBDA_PATH}" || return
 python3 --version
 sudo apt-get update
 sudo apt-get install python3-virtualenv
@@ -11,4 +11,4 @@ sudo apt-get install python3-venv
 python3 -m venv venv37
 python3 -m pip install chalice
 pip install -r requirements.txt
-chalice deploy --stage ${DEPLOY_LAMBDA_DOTENV}
+chalice deploy --stage "${DEPLOY_LAMBDA_DOTENV}"
