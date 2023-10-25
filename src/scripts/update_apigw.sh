71d97a59eb0b9f46144db3738b864129015f9905
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 19:06:36 -0300

    fix: update apigw command

--- src/scripts/update_apigw.sh
+++ src/scripts/update_apigw.sh
@@ -7,8 +7,5 @@ if test -z "$API_ID"
 then
       echo "API Gateway not exists"
 else
-      pwd
-      ls
       aws apigateway put-rest-api --rest-api-id h5jamk8fw6 --mode merge --body 'fileb://infra/terraform/swagger-spec.json'
-      aws apigateway put-rest-api --rest-api-id h5jamk8fw6 --mode merge --body 'fileb:///infra/terraform/swagger-spec.json'
 fi
