1610b49bb3f752eece36c32ec6e85b009c50a7e9
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 15:16:41 -0300

    fix: refactor build application add script to build image

--- src/scripts/build_image.sh
+++ src/scripts/build_image.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 BUILD_IMAGE_IMAGE_NAME="$(echo "${BUILD_IMAGE_IMAGE_NAME}" | circleci env subst)"
 BUILD_IMAGE_IMAGE_TAG="$(echo "${BUILD_IMAGE_IMAGE_TAG}" | circleci env subst)"
 
--- src/scripts/upload_image.sh
+++ src/scripts/upload_image.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 BUILD_IMAGE_ECR_URL="$(echo "${BUILD_IMAGE_ECR_URL}" | circleci env subst)"
 BUILD_IMAGE_IMAGE_NAME="$(echo "${BUILD_IMAGE_IMAGE_NAME}" | circleci env subst)"
 BUILD_IMAGE_IMAGE_TAG="$(echo "${BUILD_IMAGE_IMAGE_TAG}" | circleci env subst)"
