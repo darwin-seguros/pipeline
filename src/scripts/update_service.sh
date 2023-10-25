27204c389199e3d9850bee3178e74689a0ade4ca
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 15:49:59 -0300

    feat: add update service command

---
+++ src/scripts/update_service.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+UPDATE_SERVICE_DOTENV="$(echo "${UPDATE_SERVICE_DOTENV}" | circleci env subst)"
+GIT_MANIFEST_SECRET="$(echo "${GIT_MANIFEST_SECRET}" | circleci env subst)"
+UPDATE_SERVICE_ECR_URL="$(echo "${UPDATE_SERVICE_ECR_URL}" | circleci env subst)"
+UPDATE_SERVICE_IMAGE_NAME="$(echo "${UPDATE_SERVICE_IMAGE_NAME}" | circleci env subst)"
+UPDATE_SERVICE_IMAGE_TAG="$(echo "${UPDATE_SERVICE_IMAGE_TAG}" | circleci env subst)"
+UPDATE_SERVICE_TAG="$(echo "${UPDATE_SERVICE_TAG}" | circleci env subst)"
+
+node ./k8s_helpers/create_config_map.js "./.env.${UPDATE_SERVICE_DOTENV}" > configmap.yml
+
+git config --global user.email "devops@darwinseguros.com"
+git config --global user.name "DevOps"
+git clone https://${GIT_MANIFEST_SECRET}@dev.azure.com/darwin-seguros/AWS/_git/kubernete-manifest
+echo "Clone k8s manifest repo"
+
+cp -rf configmap.yml kubernete-manifest/${UPDATE_SERVICE_IMAGE_NAME}/${UPDATE_SERVICE_DOTENV}/configmap.yml
+echo "Update configmap on repository"
+
+sed -i 's/version: .*$/version: '"$(date +%Y%m%d%H%M%S)"'/g' kubernete-manifest/${UPDATE_SERVICE_IMAGE_NAME}/${UPDATE_SERVICE_DOTENV}/deployment.yml
+echo "Updated deployment on repository"
+
+aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" docker login --username AWS --password-stdin "${UPDATE_SERVICE_ECR_URL}"
+docker pull ${UPDATE_SERVICE_ECR_URL}:${UPDATE_SERVICE_IMAGE_TAG}
+docker tag ${UPDATE_SERVICE_ECR_URL}:${UPDATE_SERVICE_IMAGE_TAG} ${UPDATE_SERVICE_ECR_URL}:${UPDATE_SERVICE_TAG}
+docker push ${UPDATE_SERVICE_ECR_URL}:${UPDATE_SERVICE_TAG}
+
+cd kubernete-manifest
+git pull
+git add .
+git commit -m "Updating ${UPDATE_SERVICE_IMAGE_NAME}/${UPDATE_SERVICE_DOTENV} configmap by pipeline tag: ${CIRCLE_BUILD_NUM}"
+git push
+echo "Push changes to k8s manifest repo"
