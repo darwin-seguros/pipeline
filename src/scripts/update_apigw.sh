78ac35f65507b5d3d4bd74ca5a03457655aa460f
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-25 18:11:58 -0300

    fix: update apigw command

---
+++ src/scripts/update_apigw.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+GENERATE_APIGW_FILE_IMAGE_NAME="$(echo "${GENERATE_APIGW_FILE_IMAGE_NAME}" | circleci env subst)"
+
+echo "Running update api gateway"
+API_ID=$(aws apigateway get-rest-apis --query 'items[?name==`${GENERATE_APIGW_FILE_IMAGE_NAME}`].id')
+if test -z "$API_ID" 
+then
+      echo "API Gateway not exists"
+else
+      aws apigateway put-rest-api --rest-api-id h5jamk8fw6 --mode merge --body 'fileb:///swagger-spec.json'
+fi
