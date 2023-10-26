74104deee6f4898ebaa69322b3e71c905f436270
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-26 14:06:05 -0300

    fix: update pipeline

---
+++ src/scripts/setup_default_branchs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SETUP_DEFAULT_BRANCHS_OIDC_TOKEN="$(echo "${SETUP_DEFAULT_BRANCHS_OIDC_TOKEN}" | circleci env subst)"
+
+curl --location --request PUT "https://circleci.com/api/v1.1/project/github/darwin-seguros/search-api/settings" \
+     --header "Circle-Token: ${SETUP_DEFAULT_BRANCHS_OIDC_TOKEN}" \
+     --header "Accept: application/json" \
+     --header "Content-Type: application/json" \
+     --data "{
+         "feature_flags": {
+             "pr-only-branch-overrides": "main, develop, staging, release\\/(\\d{1,}\\.){2}\\d{1,}\\-rc\\d{1,}"
+         }
+     }"
