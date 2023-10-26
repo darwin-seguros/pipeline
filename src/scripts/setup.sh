f9fcd4b6c21e16dce4e02c29c5dcd7d3511a888d
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-26 09:45:45 -0300

    feat: add setup envs

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -2,6 +2,7 @@
 SETUP_CONFIGURATION_PATH="$(echo "${SETUP_CONFIGURATION_PATH}" | circleci env subst)"
 SETUP_JSON_FILE="$(echo "${SETUP_JSON_FILE}" | circleci env subst)"
 SETUP_LOOKUP_KEY="$(echo "${SETUP_LOOKUP_KEY}" | circleci env subst)"
+SETUP_GIT_BRANCH="$(echo "${SETUP_GIT_BRANCH}" | circleci env subst)"
 
 cp $(jq -r ".${SETUP_LOOKUP_KEY}" "${SETUP_JSON_FILE}") "${SETUP_CONFIGURATION_PATH}"
 cat "${SETUP_JSON_FILE}"
@@ -32,7 +33,7 @@ tag_main="stable"
 tag_release="latest"
 tag_staging="latest"
 
-branch=$(echo << pipeline.git.branch >> | cut -d"/" -f1)
+branch=$(echo "${SETUP_GIT_BRANCH}" >> | cut -d"/" -f1)
 echo "BRANCH = "$branch""
 
 context="contexts_$branch"
