985571f7662a70076f125082ca91afd5ae9a17f1
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-26 14:17:10 -0300

    fix: update pipeline

--- src/scripts/setup_default_branchs.sh
+++ src/scripts/setup_default_branchs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SETUP_DEFAULT_BRANCHS_OIDC_TOKEN="$(echo "${SETUP_DEFAULT_BRANCHS_OIDC_TOKEN}" | circleci env subst)"
+SETUP_PROJECT="$(echo "${SETUP_PROJECT}" | circleci env subst)"
 
-curl --location --request PUT "https://circleci.com/api/v1.1/project/github/darwin-seguros/search-api/settings" \
+curl --location --request PUT "https://circleci.com/api/v1.1/project/github/darwin-seguros/${SETUP_PROJECT}/settings" \
      --header "Circle-Token: ${SETUP_DEFAULT_BRANCHS_OIDC_TOKEN}" \
      --header "Accept: application/json" \
      --header "Content-Type: application/json" \
