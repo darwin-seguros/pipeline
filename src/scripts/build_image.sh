63342ce0303b3ee167241d2df03b6a00aae2aae7
Leandro Ferreira <ljaferreira@gmail.com>
2023-10-31 09:43:10 -0300

    Update build_image.sh

--- src/scripts/build_image.sh
+++ src/scripts/build_image.sh
@@ -2,6 +2,6 @@
 BUILD_IMAGE_IMAGE_NAME="$(echo "${BUILD_IMAGE_IMAGE_NAME}" | circleci env subst)"
 BUILD_IMAGE_IMAGE_TAG="$(echo "${BUILD_IMAGE_IMAGE_TAG}" | circleci env subst)"
 
-cat $(npm config get userconfig) >> .npmrc
+cat "$(npm config get userconfig)" >> .npmrc
 echo "Building docker image with identification: ${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}"
 docker build -t "${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}" .
