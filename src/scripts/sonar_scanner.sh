dc0b86909b516995b13ca5b0b8889a70a2c18963
Leandro Ferreira <ljaferreira@gmail.com>
2023-11-01 11:07:31 -0300

    feat: add sonar

--- src/scripts/sonar_scanner.sh
+++ src/scripts/sonar_scanner.sh
@@ -4,10 +4,10 @@ SONAR_PROJECT_KEY="$(echo "${SONAR_PROJECT_KEY}" | circleci env subst)"
 SONAR_URL="$(echo "${SONAR_URL}" | circleci env subst)"
 
 export SONAR_SCANNER_VERSION=4.7.0.2747
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
-curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
-unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_HOME="${HOME}/.sonar/sonar-scanner-${SONAR_SCANNER_VERSION}-linux"
+curl --create-dirs -sSLo "${HOME}/.sonar/sonar-scanner.zip" "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-linux.zip"
+unzip -o "${HOME}/.sonar/sonar-scanner.zip" -d "${HOME}/.sonar/"
+export PATH="${SONAR_SCANNER_HOME}/bin:${PATH}"
 export SONAR_SCANNER_OPTS="-server"
 
 
