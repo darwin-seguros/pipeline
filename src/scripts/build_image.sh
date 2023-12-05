c8a2b0681cccba8479b40c17c93f4aeccbe30af2
Leandro Ferreira <ljaferreira@gmail.com>
2023-12-05 12:24:48 -0300

    feat: add timeout test:ci

--- src/scripts/build_image.sh
+++ src/scripts/build_image.sh
@@ -5,5 +5,5 @@ BUILD_IMAGE_IMAGE_TAG="$(echo "${BUILD_IMAGE_IMAGE_TAG}" | circleci env subst)"
 cat "$(npm config get userconfig)" >> .npmrc
 cat .npmrc
 echo "Building docker image with identification: ${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}"
-SWAGGER_VERSION="${CIRCLE_BUILD_NUM}"
+echo "SWAGGER_VERSION=${CIRCLE_BUILD_NUM}" >> .env
 docker build -t "${BUILD_IMAGE_IMAGE_NAME}:${BUILD_IMAGE_IMAGE_TAG}" .
