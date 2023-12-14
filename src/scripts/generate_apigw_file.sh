7a639e58aacdbbd7df2bf743d8d46fd524188a88
Ricardo Larrahona <ricardo.larrahona@darwinseguros.com>
2023-12-14 13:48:45 -0300

    fix:fix swagger-spec

--- src/scripts/generate_apigw_file.sh
+++ src/scripts/generate_apigw_file.sh
@@ -2,6 +2,7 @@
 GENERATE_APIGW_FILE_DOTENV="$(echo "${GENERATE_APIGW_FILE_DOTENV}" | circleci env subst)"
 GENERATE_APIGW_FILE_IMAGE_NAME="$(echo "${GENERATE_APIGW_FILE_IMAGE_NAME}" | circleci env subst)"
 
+
 echo "Running generate swagger file"
 VPC_LINK_ID=$(aws apigateway get-vpc-links | jq -r '.items[0].id') \
 SWAGGER_VERSION="${CIRCLE_BUILD_NUM}" \
