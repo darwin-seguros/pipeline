9c4b5f52270a7fd13c64cd9acf68aa8e07333648
Luiz Filipe <luiz.martins@darwinseguros.com>
2024-02-16 14:15:36 -0300

    fix: fixing update service ecs

--- src/scripts/update_cluster.sh
+++ src/scripts/update_cluster.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
 UPDATE_CLUSTER="$(echo "${UPDATE_CLUSTER}" | circleci env subst)"
 UPDATE_SERVICE="$(echo "${UPDATE_SERVICE}" | circleci env subst)"
 
 echo "CLUSTER = ${UPDATE_CLUSTER}"
 echo "SERVICE = ${UPDATE_SERVICE}"
 
-aws ecs update-service --region "${AWS_DEFAULT_REGION}" --cluster "${UPDATE_CLUSTER}" --service "${UPDATE_SERVICE}" --force-new-deployment  --no-cli-pager || exit 0
+SERVICES=$(aws ecs list-services --region "${AWS_DEFAULT_REGION}" --cluster "${UPDATE_CLUSTER}" --query "serviceArns[?starts_with(@, '${UPDATE_SERVICE}')].split('/').[-1]" --output text)
+
+for SERVICE in $SERVICES; do
+    echo "Atualizando o serviço ${SERVICE}..."
+    aws ecs update-service --region "${AWS_DEFAULT_REGION}" --cluster "${UPDATE_CLUSTER}" --service "${SERVICE}" --force-new-deployment --no-cli-pager || echo "Falha ao atualizar o serviço ${SERVICE}"
+done
