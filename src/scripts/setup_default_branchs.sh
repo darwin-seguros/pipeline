3eba4b253d4a143cd827802b1f22cf4b1300d4c0
Ricardo Larrahona <ricardo.larrahona@darwinseguros.com>
2024-03-21 13:34:47 -0300

    fix:fix feature_flags

--- src/scripts/setup_default_branchs.sh
+++ src/scripts/setup_default_branchs.sh
@@ -6,4 +6,4 @@ curl --location --request PUT "https://circleci.com/api/v1.1/project/github/darw
      --header "Circle-Token: ${SETUP_DEFAULT_BRANCHS_OIDC_TOKEN}" \
      --header "Accept: application/json" \
      --header "Content-Type: application/json" \
-     --data "{\"feature_flags\":{\"pr-only-branch-overrides\":\"main, develop, staging, release\\/(\\d{1,}\\.){2}\\d{1,}\\-rc\\d{1,}\"}}"
+     --data '{"feature_flags":{"pr-only-branch-overrides":".*"}}'
